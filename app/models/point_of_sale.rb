6751a61020b6e8641ccb4385f16273936ef24249
khwesten <k-heiner@hotmail.com>
2020-04-18 14:57:50 -0300

    Change scope name from in_area to by_point

--- app/models/point_of_sale.rb
+++ app/models/point_of_sale.rb
@@ -1,3 +1,3 @@
 class PointOfSale < ActiveRecord::Base
-  scope :in_area, -> (rgeo_point) { where("ST_CONTAINS(coverage_area, ST_GeomFromText('#{rgeo_point}'))") }
+  scope :by_point, -> (rgeo_point) { where("ST_CONTAINS(coverage_area, ST_GeomFromText('#{rgeo_point}'))") }
 end
