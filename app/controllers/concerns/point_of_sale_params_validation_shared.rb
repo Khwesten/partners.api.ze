c0808f58f3f890209b9ad4428cda21ca36acdd54
khwesten <k-heiner@hotmail.com>
2020-04-18 14:56:53 -0300

    Abstract params validation to PointOfSaleParamsValidationShared

---
+++ app/controllers/concerns/point_of_sale_params_validation_shared.rb
@@ -0,0 +1,21 @@
+module PointOfSaleParamsValidationShared
+  extend ActiveSupport::Concern
+
+  VALIDATION_SEARCH_ERROR = 'lat and lng is required'
+
+  included do
+    private
+
+      def partners_params
+        params.require(:point_of_sale).permit(
+          :trading_name, :owner_name, :document, :coverage_area, :address
+        )
+      end
+
+      def validate_search_params
+        invalid_params = params[:lat].blank? || params[:lng].blank?
+
+        raise ActionController::ParameterMissing, VALIDATION_SEARCH_ERROR if invalid_params
+      end
+  end
+end
