71026d3b2661f42adb0c95dd0073b152d7523cfa
khwesten <k-heiner@hotmail.com>
2020-04-18 14:55:42 -0300

    Create a const to RESOURCE_DOES_NOT_EXIST

--- app/controllers/concerns/exception_handler_shared.rb
+++ app/controllers/concerns/exception_handler_shared.rb
@@ -1,6 +1,8 @@
 module ExceptionHandlerShared
   extend ActiveSupport::Concern
 
+  RESOURCE_DOES_NOT_EXIST = 'resource does not exists'
+
   included do
     rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_exception_handler
     rescue_from ActionController::ParameterMissing, with: :required_params_exception_handler
@@ -12,7 +14,7 @@ module ExceptionHandlerShared
       end
 
       def record_not_found_exception_handler
-        render json: { message: "resource does not exists" }, status: 404
+        render json: { message: RESOURCE_DOES_NOT_EXIST }, status: 404
       end
   end
 end
