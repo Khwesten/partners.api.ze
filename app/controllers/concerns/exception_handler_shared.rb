62da99a201915c7148970a509b79bc9daf685e5b
khwesten <k-heiner@hotmail.com>
2020-04-18 12:54:38 -0300

    Add record_not_found_exception_handler

--- app/controllers/concerns/exception_handler_shared.rb
+++ app/controllers/concerns/exception_handler_shared.rb
@@ -2,12 +2,17 @@ module ExceptionHandlerShared
   extend ActiveSupport::Concern
 
   included do
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_exception_handler
     rescue_from ActionController::ParameterMissing, with: :required_params_exception_handler
 
     protected
 
       def required_params_exception_handler(exception)
-        render json: { message: exception.message }.to_json, status: 400
+        render json: { message: exception.message }, status: 400
+      end
+
+      def record_not_found_exception_handler
+        render json: { message: "resource does not exists" }, status: 404
       end
   end
 end
