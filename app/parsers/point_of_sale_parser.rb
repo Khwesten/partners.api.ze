785af026bb7f9149a3d04d15f7c3f2412bd6a2a6
khwesten <k-heiner@hotmail.com>
2020-04-18 19:41:04 -0300

    Create parser to PointOfSale by params

---
+++ app/parsers/point_of_sale_parser.rb
@@ -0,0 +1,18 @@
+class PointOfSaleParser
+  def self.from_params(params, decoder: RGeo::GeoJSON)
+    coverage_area = params[:coverage_area].to_h
+    address = params[:address].to_h
+
+    // TODO: Make validation to check decode successfully
+    rgeo_coverage_area = decoder.decode(coverage_area)
+    rgeo_address = decoder.decode(address)
+
+    PointOfSale.new(
+      trading_name: params[:trading_name],
+      owner_name: params[:owner_name],
+      document: params[:document],
+      coverage_area: rgeo_coverage_area,
+      address: rgeo_address
+    )
+  end
+end
