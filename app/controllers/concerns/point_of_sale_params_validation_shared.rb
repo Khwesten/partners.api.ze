a8d1542a489e34e2839a59455516ccbe05df8e8a
khwesten <k-heiner@hotmail.com>
2020-04-18 19:42:22 -0300

    Fix permitted params to PointOfSale and make string as const

--- app/controllers/concerns/point_of_sale_params_validation_shared.rb
+++ app/controllers/concerns/point_of_sale_params_validation_shared.rb
@@ -1,18 +1,25 @@
 module PointOfSaleParamsValidationShared
   extend ActiveSupport::Concern
 
+  LATITUDE_CANT_BLANK_MESSAGE = "Latitude can't be blank"
+  LONGITUDE_CANT_BLANK_MESSAGE = "Longitude can't be blank"
+
   included do
     private
 
       def point_of_sale_params
+        rgeo_params_permitted = [:type, coordinates: []]
+
         params.require(:point_of_sale).permit(
-          :trading_name, :owner_name, :document, :coverage_area, :address
-        )
+          :trading_name, :owner_name, :document, coverage_area: rgeo_params_permitted, address: rgeo_params_permitted
+        ).tap do |whitelisted|
+          whitelisted[:coverage_area][:coordinates] = params[:point_of_sale][:coverage_area][:coordinates]
+        end
       end
 
       def validate_search_params
-        lat_blank_error = ParamError.new(:lat, ["Latitude can't be blank"]) if params[:lat].blank?
-        lng_blank_error = ParamError.new(:lng, ["Longitude can't be blank"]) if params[:lng].blank?
+        lat_blank_error = ParamError.new(:lat, [LATITUDE_CANT_BLANK_MESSAGE]) if params[:lat].blank?
+        lng_blank_error = ParamError.new(:lng, [LONGITUDE_CANT_BLANK_MESSAGE]) if params[:lng].blank?
 
         errors = [lat_blank_error, lng_blank_error].compact
 
