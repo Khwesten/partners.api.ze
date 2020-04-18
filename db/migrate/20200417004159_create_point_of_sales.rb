e5bc7a0952120c383730e682f15a2ae45f47809a
khwesten <k-heiner@hotmail.com>
2020-04-18 12:34:02 -0300

    Create point_of_sale migration

---
+++ db/migrate/20200417004159_create_point_of_sales.rb
@@ -0,0 +1,15 @@
+class CreatePointOfSales < ActiveRecord::Migration
+  def change
+    create_table :point_of_sales do |t|
+      t.string :trading_name, null: false
+      t.string :owner_name, null: false
+      t.string :document, null: false
+      t.multi_polygon :coverage_area, null: false
+      t.point :address, null: false
+
+      t.timestamps null: false
+
+      t.index :coverage_area, type: :spatial
+    end
+  end
+end
