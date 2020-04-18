08fbc78200492b7f97db48b58742442f06274fad
khwesten <k-heiner@hotmail.com>
2020-04-18 16:17:43 -0300

    Change response key from message to errors

--- app/controllers/concerns/exception_handler_shared.rb
+++ app/controllers/concerns/exception_handler_shared.rb
@@ -10,11 +10,11 @@ module ExceptionHandlerShared
     protected
 
       def required_params_exception_handler(exception)
-        render json: { message: exception.message }, status: 400
+        render json: { errors: exception.message }, status: 400
       end
 
       def record_not_found_exception_handler
-        render json: { message: RESOURCE_DOES_NOT_EXIST }, status: 404
+        render json: { errors: RESOURCE_DOES_NOT_EXIST }, status: 404
       end
   end
 end
