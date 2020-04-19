3183120aa65102489de682d8b9a6974a8cc2ff2f
khwesten <k-heiner@hotmail.com>
2020-04-19 16:23:52 -0300

    Create search permitted params method and refactor create params

--- app/controllers/concerns/point_of_sale_permitted_params_shared.rb
+++ app/controllers/concerns/point_of_sale_permitted_params_shared.rb
@@ -1,17 +1,28 @@
 module PointOfSalePermittedParamsShared
   extend ActiveSupport::Concern
 
+  RGEO_PERMITTED_PARAMS = [:type, coordinates: []]
+  CREATE_PERMITTED_PARAMS = [
+    :trading_name, :owner_name, :document, coverage_area: RGEO_PERMITTED_PARAMS, address: RGEO_PERMITTED_PARAMS
+  ]
+
   included do
     private
 
       def create_point_of_sale_params
-        rgeo_params_permitted = [:type, coordinates: []]
+        params.require(:point_of_sale).permit(CREATE_PERMITTED_PARAMS).tap do |whitelisted|
+          if params.dig(:point_of_sale, :coverage_area, :coordinates)
+            whitelisted[:coverage_area][:coordinates] = params[:point_of_sale][:coverage_area][:coordinates]
+          end
 
-        params.require(:point_of_sale).permit(
-          :trading_name, :owner_name, :document, coverage_area: rgeo_params_permitted, address: rgeo_params_permitted
-        ).tap do |whitelisted|
-          whitelisted[:coverage_area][:coordinates] = params[:point_of_sale][:coverage_area][:coordinates]
-        end
+          if params.dig(:point_of_sale, :address, :coordinates)
+            whitelisted[:address][:coordinates] = params[:point_of_sale][:address][:coordinates]
+          end
+        end.to_h
+      end
+
+      def search_point_of_sale_params
+        params.permit(:lat, :lng).to_h
       end
   end
 end
