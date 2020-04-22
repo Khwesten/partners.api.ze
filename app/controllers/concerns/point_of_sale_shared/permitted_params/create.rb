f4e88027808fe201b2d4391edc906016ab42da17
Khwesten <k-heiner@hotmail.com>
2020-04-21 21:32:17 -0300

    Improve readability from create permitted params

--- app/controllers/concerns/point_of_sale_shared/permitted_params/create.rb
+++ app/controllers/concerns/point_of_sale_shared/permitted_params/create.rb
@@ -1,7 +1,7 @@
 module PointOfSaleShared::PermittedParams::Create
   extend ActiveSupport::Concern
 
-  RGEO_PERMITTED_PARAMS = [:type, coordinates: []]
+  RGEO_PERMITTED_PARAMS = [:type, coordinates: [[[]]]]
   CREATE_PERMITTED_PARAMS = [
     :trading_name, :owner_name, :document, coverage_area: RGEO_PERMITTED_PARAMS, address: RGEO_PERMITTED_PARAMS
   ]
@@ -11,11 +11,11 @@ module PointOfSaleShared::PermittedParams::Create
 
       def create_point_of_sale_params
         params.permit(CREATE_PERMITTED_PARAMS).tap do |whitelisted|
-          if params.dig(:coverage_area, :coordinates)
-            whitelisted[:coverage_area][:coordinates] = params[:coverage_area][:coordinates]
-          end
+          address_coordinates = params.dig(:address, :coordinates)
+          coverage_coordinates = params.dig(:coverage_area, :coordinates)
 
-          whitelisted[:address][:coordinates] = params[:address][:coordinates] if params.dig(:address, :coordinates)
+          whitelisted[:address][:coordinates] = address_coordinates if address_coordinates
+          whitelisted[:coverage_area][:coordinates] = coverage_coordinates if coverage_coordinates
         end.to_h
       end
   end
